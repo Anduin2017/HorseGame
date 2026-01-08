7707e097ac185db6ed87410b922ddc438c971da5
Anduin Xue <anduin@aiursoft.com>
2026-01-08 18:41:15 +0000

    Fix lint.

--- tests/HorseGame.Tests/ClueGeneratorTests.cs
+++ tests/HorseGame.Tests/ClueGeneratorTests.cs
@@ -12,7 +12,7 @@ public class ClueGeneratorTests
     public void SuitableGameGenerator_Generates10Levels()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.GenerateNewGame();
+        var game = generator.BuildSuitable();
 
         Assert.Equal(10, game.Levels.Count);
     }
@@ -21,13 +21,12 @@ public class ClueGeneratorTests
     public void NotFirstGenerator_GeneratesCluesForNonWinners()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.GenerateNewGame();
+        var game = generator.BuildSuitable();
         var clueGen = new NotFirstGenerator();
 
         var clues = clueGen.GetClues(game).ToList();
 
-        // Should have 3 NotFirst clues per round (for non-winners)
-        Assert.Equal(30, clues.Count); // 10 rounds * 3 non-winners
+        Assert.Equal(30, clues.Count);
         Assert.All(clues, c => Assert.IsType<NotFirst>(c));
     }
 
@@ -35,13 +34,12 @@ public class ClueGeneratorTests
     public void TopTwoGenerator_GeneratesCluesForTop2()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.GenerateNewGame();
+        var game = generator.BuildSuitable();
         var clueGen = new TopTwoGenerator();
 
         var clues = clueGen.GetClues(game).ToList();
 
-        // Should have 2 TopTwo clues per round
-        Assert.Equal(20, clues.Count); // 10 rounds * 2
+        Assert.Equal(20, clues.Count);
         Assert.All(clues, c => Assert.IsType<TopTwo>(c));
     }
 
@@ -49,13 +47,12 @@ public class ClueGeneratorTests
     public void TopThreeGenerator_GeneratesCluesForTop3()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.GenerateNewGame();
+        var game = generator.BuildSuitable();
         var clueGen = new TopThreeGenerator();
 
         var clues = clueGen.GetClues(game).ToList();
 
-        // Should have 3 TopThree clues per round
-        Assert.Equal(30, clues.Count); // 10 rounds * 3
+        Assert.Equal(30, clues.Count);
         Assert.All(clues, c => Assert.IsType<TopThree>(c));
     }
 
@@ -63,12 +60,12 @@ public class ClueGeneratorTests
     public void ClueService_GeneratesRandomizedClues()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.GenerateNewGame();
+        var game = generator.BuildSuitable();
         var service = new Unified.Services.ClueService();
 
         var clues = service.GenerateClues(game);
 
         Assert.NotEmpty(clues);
-        Assert.True(clues.Count > 50); // Should have many clues
+        Assert.True(clues.Count > 50);
     }
 }
