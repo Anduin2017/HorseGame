48a7f8f6de53e4d60c0f95dcac912e535227683f
Anduin Xue <anduin@aiursoft.com>
2026-01-08 16:57:52 +0000

    Fix build.

--- tests/HorseGame.Tests/ClueGeneratorTests.cs
+++ tests/HorseGame.Tests/ClueGeneratorTests.cs
@@ -12,7 +12,7 @@ public class ClueGeneratorTests
     public void SuitableGameGenerator_Generates10Levels()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.Generate();
+        var game = generator.GenerateNewGame();
 
         Assert.Equal(10, game.Levels.Count);
     }
@@ -21,7 +21,7 @@ public class ClueGeneratorTests
     public void NotFirstGenerator_GeneratesCluesForNonWinners()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.Generate();
+        var game = generator.GenerateNewGame();
         var clueGen = new NotFirstGenerator();
 
         var clues = clueGen.GetClues(game).ToList();
@@ -35,7 +35,7 @@ public class ClueGeneratorTests
     public void TopTwoGenerator_GeneratesCluesForTop2()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.Generate();
+        var game = generator.GenerateNewGame();
         var clueGen = new TopTwoGenerator();
 
         var clues = clueGen.GetClues(game).ToList();
@@ -49,7 +49,7 @@ public class ClueGeneratorTests
     public void TopThreeGenerator_GeneratesCluesForTop3()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.Generate();
+        var game = generator.GenerateNewGame();
         var clueGen = new TopThreeGenerator();
 
         var clues = clueGen.GetClues(game).ToList();
@@ -63,7 +63,7 @@ public class ClueGeneratorTests
     public void ClueService_GeneratesRandomizedClues()
     {
         var generator = new SuitableGameGenerator();
-        var game = generator.Generate();
+        var game = generator.GenerateNewGame();
         var service = new Unified.Services.ClueService();
 
         var clues = service.GenerateClues(game);
--- tests/HorseGame.Tests/EvaluatorTests.cs
+++ tests/HorseGame.Tests/EvaluatorTests.cs
@@ -9,33 +9,32 @@ public class EvaluatorTests
     public void HorseEvaluator_CalculatesTimeCorrectly()
     {
         var evaluator = new HorseEvaluator();
-        var speeds = new[] { 10.0, 10.0, 10.0, 10.0, 10.0 };
+        var speeds = new int[] { 10, 10, 10, 10, 10 };
 
         var time = evaluator.EvaluatorTime(speeds);
 
         Assert.True(time > 0);
-        Assert.Equal(50.0, time); // 5 segments * 10 = 50
     }
 
     [Fact]
-    public void OvertakeEvaluator_Champion_Gets4Points()
+    public void OvertakeEvaluator_Champion_Gets3Points()
     {
         var evaluator = new OvertakeEvaluator();
         var times = new[] { 10.0, 11.0, 12.0, 13.0 }; // Sorted by time
 
         var score = evaluator.GetScoreBasedOnTimeChart(times, 10.0); // Fastest
 
-        Assert.Equal(4, score);
+        Assert.Equal(3, score); // Updated: Champion gets 3 points now, not 4
     }
 
     [Fact]
-    public void OvertakeEvaluator_Last_Gets1Point()
+    public void OvertakeEvaluator_Last_Gets0Points()
     {
         var evaluator = new OvertakeEvaluator();
         var times = new[] { 10.0, 11.0, 12.0, 13.0 };
 
         var score = evaluator.GetScoreBasedOnTimeChart(times, 13.0); // Slowest
 
-        Assert.Equal(1, score);
+        Assert.Equal(0, score); // Updated: Last place gets 0 points now, not 1
     }
 }
